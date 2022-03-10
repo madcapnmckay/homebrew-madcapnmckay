e057191f014516df212d33baf0bef3f1c1275ed7
Ian Mckay <ian@avastmehearties.com>
2022-03-10 12:29:05 -0800

    Update sha

--- Casks/xquartz-legacy.rb
+++ Casks/xquartz-legacy.rb
@@ -1,6 +1,6 @@
 cask "xquartz-legacy" do
   version "2.7.7"
-  sha256 "ba49724bc588fde16a988b8e6f325f1fcf0d18de"
+  sha256 "c9b3a373b7fd989331117acb9696fffd6b9ee1a08ba838b02ed751b184005211"
 
   url "https://github.com/XQuartz/XQuartz/releases/download/XQuartz-2.7.7/XQuartz-2.7.7.dmg"
 
