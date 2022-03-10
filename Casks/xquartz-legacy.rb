b82b5e1b3f84ebb808a12b92de5f96e92da555a2
Ian Mckay <ian@avastmehearties.com>
2022-03-10 12:22:24 -0800

    Fixed whitespace

--- Casks/xquartz-legacy.rb
+++ Casks/xquartz-legacy.rb
@@ -1,12 +1,12 @@
 cask "xquartz-legacy" do
-    version "2.7.7"
-    sha256 "ba49724bc588fde16a988b8e6f325f1fcf0d18de"
-    
-    url "https://github.com/XQuartz/XQuartz/releases/download/XQuartz-2.7.7/XQuartz-2.7.7.dmg"
+	version "2.7.7"
+	sha256 "ba49724bc588fde16a988b8e6f325f1fcf0d18de"
 
-    name "XQuartz"
-    desc "Open-source version of the X.Org X Window System"
-    homepage "https://www.xquartz.org/"
+	url "https://github.com/XQuartz/XQuartz/releases/download/XQuartz-2.7.7/XQuartz-2.7.7.dmg"
 
-    pkg "XQuartz.pkg"
-  end
+	name "XQuartz"
+	desc "Open-source version of the X.Org X Window System"
+	homepage "https://www.xquartz.org/"
+
+	pkg "XQuartz.pkg"
+end
